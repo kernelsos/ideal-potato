3cae7625f0d76eb1070ddb0ff2cda4a2ae18a61f
kernelsos <gagansharma800t.com>
2026-04-05 22:46:57 +0530

    Initial commit

---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { SessionContext, SessionProvider } from "next-auth/react"
+
+export function Providers({children}: {
+    children: React.ReactNode
+}) {
+    return <SessionProvider>
+        {children}
+    </SessionProvider>
+}
