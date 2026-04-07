8f5d0fcb8c69e52d22307c3f77b8e0c86d37564d
kernelsos <gagansharma800t.com>
2026-04-07 16:22:27 +0530

    Added Regex and zod code

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,2 +1,44 @@
+import { prismaClient } from "@/app/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const YT_REGEX = new RegExp("")
 
 
+const CreateStreamSchema = z.object({
+    creatorId: z.string(),
+    url: z.string()
+})
+
+
+export async function POST(req: NextRequest) {
+    try {
+        const data =CreateStreamSchema.parse(await req.json());
+        const isYt = YT_REGEX.test(data.url);
+        if (!isYt){
+            return NextResponse.json({
+            message: "Wrong URL format"
+        },{
+            status: 411
+        })
+
+        }
+
+        const extractedId = data.url.split("?v=")[1];
+        
+        await prismaClient.stream.create({
+            data: {
+                userId: data.creatorId,
+                url: data.url,
+                extractedId,
+                type: "Youtube",
+            }
+        })
+    } catch (e) {
+        return NextResponse.json({
+            message: "Eror while adding a stream."
+        },{
+            status: 411
+        })
+    }
+}
