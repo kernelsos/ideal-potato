ee806e928ce468df267954cb8b783d1749deb573
kernelsos <gagansharma800t.com>
2026-04-09 13:24:40 +0530

    Added regex

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -2,7 +2,7 @@ import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-const YT_REGEX = new RegExp("")
+var YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
 
 
 const CreateStreamSchema = z.object({
@@ -13,28 +13,33 @@ const CreateStreamSchema = z.object({
 
 export async function POST(req: NextRequest) {
     try {
-        const data =CreateStreamSchema.parse(await req.json());
-        const isYt = YT_REGEX.test(data.url);
+        const data = CreateStreamSchema.parse(await req.json());
+        const isYt = data.url.match(YT_REGEX);
         if (!isYt){
             return NextResponse.json({
-            message: "Wrong URL format"
+                message: "Wrong URL format"
         },{
             status: 411
         })
+    }
+        //const extractedId = data.url.split("?v=")[1];
+        const extractedId = data.url.split("?v=")[1] ?? data.url.split("?si=")[1];
 
-        }
-
-        const extractedId = data.url.split("?v=")[1];
-        
-        await prismaClient.stream.create({
+        const stream = await prismaClient.stream.create({
             data: {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
                 type: "Youtube",
             }
+        });
+        return NextResponse.json({
+            message: "Added stream",
+            id: stream.id
         })
+
     } catch (e) {
+        console.log(e)
         return NextResponse.json({
             message: "Eror while adding a stream."
         },{
